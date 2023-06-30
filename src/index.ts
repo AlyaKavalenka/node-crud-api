728ff0c45378592e0968a2bb7fd28e134b8d3b23
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-30 14:05:24 +0300

    init: create web server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import http from 'http';
+
+const hostname = 'localhost';
+const port = 4000;
+
+const server = http.createServer((_, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+export default server;
